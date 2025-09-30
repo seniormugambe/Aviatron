d10164f8c51140eeea26a011ff43619e58f2953b
Developer X <105511802+seniormugambe@users.noreply.github.com>
2025-09-30 16:07:54 +0300

    Remove incorrect react-map-gl alias

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'react-map-gl': 'react-map-gl/dist/esm/index.js'
-    }
-  },
   optimizeDeps: {
     exclude: ['lucide-react', 'react-map-gl'],
   },
